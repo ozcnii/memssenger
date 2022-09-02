73c97fa8ba6b4ade925c0a4c7b19b20523f28bb1
ozcnii <ercount89@gmail.com>
2022-09-02 14:32:42 +0300

    refactor Alert component

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -2,37 +2,33 @@ import s from "./Alert.module.css";
 import ReactDOM from "react-dom";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion/dist/framer-motion";
-export default function Alert({ message, closeAlert }) {
-  return ReactDOM.createPortal(
-    <Modal message={message} closeAlert={closeAlert} />,
-    document.body
-  );
+
+export default function Alert(props) {
+  return ReactDOM.createPortal(<AlertComponent {...props} />, document.body);
 }
 
-function Modal({ message, closeAlert }) {
+const ALERT_SHOW_TIME = 4000;
+
+function AlertComponent({ message, closeAlert }) {
   const [isClosed, setIsClosed] = useState(false);
 
   useEffect(() => {
     const showTimeout = setTimeout(() => {
       setIsClosed(true);
-    }, 4000);
-    const closeTimeout = closeAlert
-      ? setTimeout(() => {
-          closeAlert(false);
-        }, 4300)
-      : 0;
+    }, ALERT_SHOW_TIME);
 
-    return () => {
-      clearTimeout(showTimeout);
-      clearTimeout(closeTimeout);
-    };
+    return () => clearTimeout(showTimeout);
   }, [closeAlert]);
 
   return (
     <div className={s.wrapper}>
-      <motion.div initial={{ x: "100%" }} animate={{ x: "0%" }}>
-        <div className={isClosed ? s.alert + " " + s.close : s.alert}>
-          <div className={s.container}>{message}</div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={!isClosed ? { opacity: 1 } : ""}
+        transition={{ duration: 0.2 }}
+      >
+        <div className={s.alert}>
+          <span className={s.container}>{message}</span>
         </div>
       </motion.div>
     </div>
